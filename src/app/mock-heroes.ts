0e4529e76d02bcb7d102c019fb3023244019fd99
LGLopez <darkdestruktion@gmail.com>
2020-11-08 21:05:17 -0600

    Parte 2 terminada

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Dr Nice', points: 21 },
+  { id: 12, name: 'Narco', points: 29 },
+  { id: 13, name: 'Bombasto', points: 15 },
+  { id: 14, name: 'Celeritas', points: 53 },
+  { id: 15, name: 'Magneta', points: 12 },
+  { id: 16, name: 'RubberMan', points: 67 },
+  { id: 17, name: 'Dynama', points: 45 },
+  { id: 18, name: 'Dr IQ', points: 57 },
+  { id: 19, name: 'Magma', points: 24 },
+  { id: 20, name: 'Tornado', points: 65 }
+];
+
